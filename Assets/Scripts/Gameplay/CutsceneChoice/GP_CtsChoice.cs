f0712184f9717394372bfbb00a815acf4411cfe4
Fikri Ahmad <rukimevick@gmail.com>
2026-06-21 19:16:19 +0700

    [Update] Environment Fixes
    :
    - Collider Trigernya mati njir

--- Assets/Scripts/Gameplay/CutsceneChoice/GP_CtsChoice.cs
+++ Assets/Scripts/Gameplay/CutsceneChoice/GP_CtsChoice.cs
@@ -4,8 +4,16 @@ using Unity.Cinemachine;
 
 public class GP_CtsChoice : MonoBehaviour
 {
+    [Tooltip("The first choice button.")]
     public Button choice1;
+    [Tooltip("The second choice button.")]
     public Button choice2;
+    [Tooltip("The target object for the first choice.")]
+    public GameObject choice1Target;
+    [Tooltip("The target object for the second choice.")]
+    public GameObject choice2Target;
+
+    [Tooltip("The Cinemachine virtual camera to switch to.")]
     public CinemachineVirtualCameraBase vcam;
 
     private void Start()
@@ -26,16 +34,24 @@ public class GP_CtsChoice : MonoBehaviour
         if (selectedButton == choice1)
         {
             Debug.Log("<color=green>Choice 1 selected!</color>");
+            if (choice1Target != null)
+            {
+                vcam.LookAt = choice1Target.transform;
+            }
         }
         else if (selectedButton == choice2)
         {
             Debug.Log("<color=blue>Choice 2 selected!</color>");
+            if (choice2Target != null)
+            {
+                vcam.LookAt = choice2Target.transform;
+            }
         }
 
         // Switch camera
         if (vcam != null)
         {
-            _Sys_VCamPriorityController vcamController = FindObjectOfType<_Sys_VCamPriorityController>();
+            _Sys_VCamPriorityController vcamController = FindAnyObjectByType<_Sys_VCamPriorityController>();
             if (vcamController != null)
             {
                 vcamController.SetCameraActive(vcam);
